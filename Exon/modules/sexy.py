a5608fa4548ebeb5ee6d29352f5922ccffff349d
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-28 21:54:47 +0530

    Create sexy.py

---
+++ Exon/modules/sexy.py
@@ -0,0 +1,113 @@
+import random
+
+from telethon import Button, events
+
+from .. import telethn as asst
+
+BUTTON = [[Button.url("✨ Sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/Mega_Bots_Support")]]
+HOT = "https://telegra.ph/file/daad931db960ea40c0fca.gif"
+SMEXY = "https://telegra.ph/file/a23e9fd851fb6bc771686.gif"
+LEZBIAN = "https://telegra.ph/file/5609b87f0bd461fc36acb.gif"
+BIGBALL = "https://i.gifer.com/8ZUg.gif"
+LANG = "https://telegra.ph/file/423414459345bf18310f5.gif"
+CUTIE = "https://telegra.ph/file/86690281c538ce85efd3d.mp4"
+SIG = "https://telegra.ph/file/2c1190751777576cedbb9.mp4"
+BAT = "https://telegra.ph/file/a5b0781ebe5b3d6988f85.mp4"
+
+@asst.on(events.NewMessage(pattern="/horny ?(.*)"))
+async def horny(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    HORNY = f"**🔥** {mention} **ɪꜱ** {mm}**% ʜᴏʀɴʏ!**"
+    await e.reply(HORNY, buttons=BUTTON, file=HOT)
+
+
+@asst.on(events.NewMessage(pattern="/gay ?(.*)"))
+async def gay(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    GAY = f"**🍷** {mention} **ɪꜱ** {mm}**% ɢᴀʏ!**"
+    await e.reply(GAY, buttons=BUTTON, file=SMEXY)
+
+
+@asst.on(events.NewMessage(pattern="/lezbian ?(.*)"))
+async def lezbian(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    FEK = f"**💜** {mention} **ɪꜱ** {mm}**% ʟᴇᴢʙɪᴀɴ!**"
+    await e.reply(FEK, buttons=BUTTON, file=LEZBIAN)
+
+
+@asst.on(events.NewMessage(pattern="/boob ?(.*)"))
+async def boob(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    BOOBS = f"**🍒** {mention}**'ꜱ ʙᴏᴏʙꜱ ꜱɪᴢᴇ ɪᴢ** {mm}**!**"
+    await e.reply(BOOBS, buttons=BUTTON, file=BIGBALL)
+
+
+@asst.on(events.NewMessage(pattern="/cock ?(.*)"))
+async def cock(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    COCK = f"**🍆** {mention}**'ꜱ ᴄᴏᴄᴋ ꜱɪᴢᴇ ɪᴢ** {mm}**ᴄᴍ**"
+    await e.reply(COCK, buttons=BUTTON, file=LANG)
+
+
+@asst.on(events.NewMessage(pattern="/cute ?(.*)"))
+async def cute(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    CUTE = f"**🍑** {mention} {mm}**% ᴄᴜᴛᴇ**"
+    await e.reply(CUTE, buttons=BUTTON, file=CUTIE)
+
+
+@asst.on(events.NewMessage(pattern="/sigma ?(.*)"))
+async def sigma(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    SIGMA = f"**🗿** {mention} **ɪꜱ** {mm}**% ꜱɪɢᴍᴀ!**"
+    await e.reply(SIGMA, buttons=BUTTON, file=SIG)
+
+
+@asst.on(events.NewMessage(pattern="/batman ?(.*)"))
+async def batman(e):
+    user_id = e.sender.id
+    user_name = e.sender.first_name
+    mention = f"[{user_name}](tg://user?id={str(user_id)})"
+    mm = random.randint(1, 100)
+    BATMAN = f"**🦇** {mention} **ɪꜱ** {mm}**% ʙᴀᴛᴍᴀɴ!**"
+    await e.reply(BATMAN, buttons=BUTTON, file=BAT)
+
+
+__help__ = """
+➻ /horny - ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ʜᴏʀɴʏᴇꜱꜱ
+
+➻ /gay - ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ɢᴜʏɴᴇꜱꜱ
+
+➻ /lezbian - ᴄʜᴇᴄᴋ ᴜʀ ᴄᴜʀʀᴇɴᴛ ʟᴀᴢʙɪᴀɴ
+
+➻ /boob - ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ʙᴏᴏʙꜱ ꜱɪᴢᴇ
+
+➻ /cute - ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ᴄᴜᴛᴇɴᴇꜱꜱ
+
+➻ /sigma - ᴄʜᴇᴄᴋ ʏᴏᴜʀ ᴄᴜʀʀᴇɴᴛ ꜱɪɢᴍᴀɴᴇꜱ
+
+➻ /batman - ᴄʜᴇᴄᴋ ʜᴏᴡ ᴍᴜᴄʜ ɢᴏᴛʜᴀᴍ ɴᴇᴇᴅꜱ ʏᴏᴜ
+"""
+
+__mod_name__ = "𝚂ᴇxʏ"
