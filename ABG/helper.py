6c5b54e58b27808dada083f03381295de0480f3e
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-29 09:19:16 +0530

    Update helper.py

--- ABG/helper.py
+++ ABG/helper.py
@@ -1,6 +1,6 @@
-HELP_IMG = ["https://telegra.ph/file/0eea314e1f17fbb08a138.png"]
+HELP_IMG = ["https://telegra.ph/file/5391d1696decca2621575.jpg"]
 
-START_IMG = ["https://telegra.ph/file/0eea314e1f17fbb08a138.png"]
+START_IMG = ["https://telegra.ph/file/db7944c238c4c9b5044ce.jpg"]
 
 
-PHOTO = ["https://telegra.ph/file/0eea314e1f17fbb08a138.png"]
+PHOTO = ["https://telegra.ph/file/8c946318ce9b14c673864.jpg"]
