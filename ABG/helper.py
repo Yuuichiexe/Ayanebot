dbb86c86e01a666545d1c9dcf949397bd8f32331
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-29 10:05:59 +0530

    Update helper.py

--- ABG/helper.py
+++ ABG/helper.py
@@ -3,4 +3,4 @@ HELP_IMG = ["https://telegra.ph/file/5391d1696decca2621575.jpg"]
 START_IMG = ["https://telegra.ph/file/db7944c238c4c9b5044ce.jpg"]
 
 
-PHOTO = ["https://telegra.ph/file/8c946318ce9b14c673864.jpg"]
+PHOTO = ["https://telegra.ph/file/963c575214588bf3090cf.jpg"]
