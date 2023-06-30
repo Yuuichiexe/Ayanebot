52bd79e3fe2923de048550588dc81e5383623145
Yuuichiexe <137192221+Yuuichiexe@users.noreply.github.com>
2023-06-30 09:57:36 +0530

    Update sexy.py

--- Exon/modules/sexy.py
+++ Exon/modules/sexy.py
@@ -4,7 +4,7 @@ from telethon import Button, events
 
 from .. import telethn as asst
 
-BUTTON = [[Button.url("🍂 Sᴜᴘᴘᴏʀᴛ 🍂", "https://t.me/Mega_Bots_Support")]]
+BUTTON = [[Button.url("✨ Sᴜᴘᴘᴏʀᴛ ✨", "https://t.me/Mega_Bots_Support")]]
 HOT = "https://telegra.ph/file/daad931db960ea40c0fca.gif"
 SMEXY = "https://telegra.ph/file/a23e9fd851fb6bc771686.gif"
 LEZBIAN = "https://telegra.ph/file/5609b87f0bd461fc36acb.gif"
